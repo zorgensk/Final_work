2f7aed28b6751efdf55b8f038b87bd1b93534e13
zorgensk <zorgensk@gmail.com>
2022-12-22 20:39:06 +0700

    решение в основной ветке

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] arr) // Метод вывода массива в терминал
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+string userInput = "hello friend how are you ? =)";
+string[] arrUser = userInput.Split(' ', ',', '.', ';').ToArray();
+
+string temp = String.Empty;
+for (int i = 0; i < arrUser.Length; i++)
+{
+    if (arrUser[i].Length <= 3)
+        temp = temp + " " + arrUser[i];
+}
+
+string[] result = temp.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+PrintArray(result);
