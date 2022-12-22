a5d900e9255dad95bd9ce5865835130ee2c5eb22
zorgensk <zorgensk@gmail.com>
2022-12-22 20:41:12 +0700

    альтернативное решение во второй ветке

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] arr) // Метод вывода массива в терминал
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+string s = "hello friend how are you ? =)";
+string []arr = s.Split().ToArray();
+var newarr = arr.Where(x => x.Length <= 3).ToArray();
+
+ PrintArray(newarr);
